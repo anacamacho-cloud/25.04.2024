8708307d56965822e3310035cfa714ea70e4928a
ana.camacho <129320167+anacamacho-cloud@users.noreply.github.com>
2024-04-25 12:15:40 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,44 @@
+class Objeto {
+    constructor(horario,data,temperatura){
+        this.horarioAtual = horario;
+        this.dataAtual = hoje;
+        this.temperaturaAtual = temperatura;
+    }
+
+    
+
+    queHorasSao(){
+        return this.horarioAtual;
+    }
+
+    queData(){
+        return this.hojeAtual;
+    }
+
+    queTemperatura(){
+        return this.temperaturaAtual;
+    }
+}
+
+
+class Objeto2 {
+    constructor(horario,data,temperatura){
+        this.giovani = horario;
+        this.nadyeska = hoje;
+        this.bernardo = temperatura;
+    }
+
+    
+
+    queHorasSao(){
+        return this.giovani;
+    }
+
+    queData(){
+        return this.nadyeska;
+    }
+
+    queTemperatura(){
+        return this.bernardo;
+    }
+}
